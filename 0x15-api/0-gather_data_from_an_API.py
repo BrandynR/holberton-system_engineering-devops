b3fa6dd53d985eab16f5ae9c6ce46b2765dbedac
BrandynR <BrandynR@github.com>
2020-02-04 04:04:03 +0000

    Fixed indent

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,17 +10,31 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com"
-    user = requests.get(url + "/users", params={"id": sys.argv[1]})
-    for names in user.json():
-        user_id = names.get('id')
-        todo = requests.get(url + "/todos", params={"userId": user_id})
-        task_complete = 0
-        tasks_array = []
-        for tasks in todo.json():
-            if tasks.get('completed') is True:
-                task_complete += 1
-                tasks_array.append(tasks.get('title'))
-        print("Employee {:s} is done with tasks({:d}/{:d}):\n\t {}".
-              format(names.get('name'), task_complete,
-                     len(todo.json()), "\n\t ".join(tasks_array)))
+    idE = int(sys.argv[1])
+    # this pulls up the posts
+    r = requests.get('https://jsonplaceholder.typicode.com/users')
+    listy = r.json()
+    for i in listy:
+        if i['id'] == idE:
+            rightdict = i
+            break
+    EMPLOYEE_NAME = rightdict["name"]
+    title_list = []
+    a = requests.get('https://jsonplaceholder.typicode.com/todos')
+    TOTAL_NUMBER_OF_TASKS = 0
+    NUMBER_OF_DONE_TASKS = 0
+    listy = a.json()
+    for i in listy:
+        if i['userId'] == idE:
+            TOTAL_NUMBER_OF_TASKS += 1
+            if i["completed"] is True:
+                NUMBER_OF_DONE_TASKS += 1
+                title_list.append(i["title"])
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        EMPLOYEE_NAME,
+        NUMBER_OF_DONE_TASKS,
+        TOTAL_NUMBER_OF_TASKS))
+    for title in title_list:
+        TASK_TITLE = "\t {}".format(title)
+        print(TASK_TITLE)
