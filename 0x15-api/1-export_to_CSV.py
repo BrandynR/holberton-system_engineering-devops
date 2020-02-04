a45e6d0b996ca1dfe1f996fad332ef5628e3e404
BrandynR <BrandynR@github.com>
2020-02-04 04:53:31 +0000

    TAsk added

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,8 +4,6 @@ Records all tasks that are owned by this employee
 Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
 File name must be: USER_ID.csv
 """
-
-
 import csv
 import requests
 import sys
