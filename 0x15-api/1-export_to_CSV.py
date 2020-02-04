822129a0562f42a83b7b368d38a4993a23460e28
BrandynR <BrandynR@github.com>
2020-02-04 04:55:28 +0000

    Added Task

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,15 +10,19 @@ import sys
 
 
 if __name__ == "__main__":
-    root = "https://jsonplaceholder.typicode.com"
-    users = requests.get(root + "/users", params={"id": sys.argv[1]})
-    for names in users.json():
-        usr_id = names.get('id')
-        todo = requests.get(root + "/todos", params={"userId": usr_id})
-        csv_arr = []
-        with open(sys.argv[1] + ".csv", 'a') as f:
-            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-            for tasks in todo.json():
-                writer.writerow([names.get('id'), names.get('name'),
-                                 str(tasks.get('completed')),
-                                 tasks.get('title')])
+    new_dict = {}
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(argv[1])).json()
+    url = "https://jsonplaceholder.typicode.com/todos"
+    with open("{}.csv".format(argv[1]), "w") as csvfile:
+        fieldnames = ["userId", "username", "completed", "title"]
+        writer = csv.DictWriter(csvfile,
+                                fieldnames=fieldnames,
+                                quoting=csv.QUOTE_ALL)
+        for task in requests.get(url).json():
+            if user["id"] == task["userId"]:
+                for k, v in task.items():
+                    if k != "id":
+                        new_dict[k] = str(v)
+                new_dict["username"] = user["username"]
+                writer.writerow(new_dict)
