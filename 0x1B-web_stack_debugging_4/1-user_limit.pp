e13aff8f8df2a7cc669bfa59283049975ba09f9b
BrandynR <BrandynR@github.com>
2020-02-27 20:45:52 +0000

    Add holberton as a user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,4 @@
+#Change the OS configuration so that it is possible to login with the holberton user 
 exec {'increase':
     command => 'sudo sed -i "s/holberton hard nofile 5\nholberton softnofile 4/holberton hard nofile 5000\nholberton soft nofile 4000" /etc/security/limits.conf;',  # lint:ignore:140chars
     path    => '/etc/security/limits.conf:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
